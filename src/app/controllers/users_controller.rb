7812652a425a09203107afaa0bd19e72f47027e2
hayato <hayato.drsp@gmail.com>
2021-12-20 14:10:06 +0900

    feature/#41_Implement_pagenation_on_various_index_pages

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: %i[edit update destroy]
 
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(10)
   end
 
   def show; end
--- src/db/seeds/menus.rb
+++ src/db/seeds/menus.rb
@@ -13,3 +13,83 @@ Menu.create!(shop_id: shop_id,
              name: name,
              price: price,
              evaluation: evaluation)
+name = 'ダークモカチップフラペチーノ'
+price = 440
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = '抹茶クリームフラペチーノ'
+price = 540
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = 'カフェミスト'
+price = 320
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = 'バニラクリームフラペチーノ'
+price = 540
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = 'コーヒージェリーフラペチーノ'
+price = 540
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+
+# ドトール
+shop_id = 2
+name = 'アイスコーヒー'
+price = 300
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = '沖縄黒糖ラテ'
+price = 300
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = '抹茶ラテタピオカ入り'
+price = 300
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = 'ホットコーヒー'
+price = 300
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = 'チョコクッキー'
+price = 300
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
+name = 'ミルクレープ'
+price = 300
+evaluation = 5
+Menu.create!(shop_id: shop_id,
+             name: name,
+             price: price,
+             evaluation: evaluation)
